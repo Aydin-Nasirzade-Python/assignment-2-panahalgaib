84ed6e171b9e5228fe67e9dc3c147f790b591518
panahalgaib <165551344+panahalgaib@users.noreply.github.com>
2024-04-07 12:20:58 -0700

    Update Task_6.py

--- Task_6.py
+++ Task_6.py
@@ -2,11 +2,11 @@
 
 def main():
   x=float(input("Enter x: "))
-  y=float (input ("Enter y: "))
-  if x*2+y*2>4 and 0<=x<=2 and y>=0 and y<x:
-      print('The point is in the shaded area')
+  y=float(input("Enter y: "))
+  if x**2+y**2>4 and 0<y<x and x<2:
+      print("The point is in the shaded area")
   else:
-      print('The point is not in the shaded area'
+      print("The point is not in the shaded area")
   pass
 
 if __name__ == "__main__":
