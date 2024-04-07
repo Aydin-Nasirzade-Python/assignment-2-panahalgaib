71adcf6e013e6a22e926eecc8af9ad85f9da729b
panahalgaib <165551344+panahalgaib@users.noreply.github.com>
2024-04-07 12:13:16 -0700

    Update Task_9.py

--- Task_9.py
+++ Task_9.py
@@ -1,9 +1,12 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  x=float(input("Enter x: "))
+  y=float (input("Enter y: "))
+  if x*2+y2<=1 and y>=-x and y>=x and y>=0 or x2+y2>=1 and y<=-x and y<=x and y<=0 or x2+y*2<=1 and y<=0 and (y>=-x or y>=х) :
+      print( 'The point is in the shaded area')
+  else:
+      print('The point is not in the shaded area')
   pass
 
 if __name__ == "__main__":
