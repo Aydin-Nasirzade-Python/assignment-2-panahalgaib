988111ec21ef307ecb9f4d3bad00c9baf241b0a8
panahalgaib <165551344+panahalgaib@users.noreply.github.com>
2024-04-07 07:30:11 -0700

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,9 +1,19 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  a="AprilMay"
+  b="JulyAugust"
+  c="OctoberNovember"
+  k=input("Enter name of the month [ex. June]: ")    
+  t=int(input("Enter the day: "))
+  if k=="March" and t>=20 or k=="June" and t<21 or k in a:
+      print(f"{k} {t} is in Spring")
+  elif k=="June" and t>=21 or k=="September" and t<22 or k in b:
+      print(f"{k} {t} is in Summer")
+  elif k=="September" and t>=22 or k=="December" and t<=21 or k in c:
+      print(f"{k} {t} is in Fall")
+  else:
+      print(f"{k} {t} is in Winter") 
   pass
 
 if __name__ == "__main__":
